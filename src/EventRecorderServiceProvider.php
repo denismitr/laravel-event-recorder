31997ad8063a6ddd15147f12b662a204af238b07
Denis Mitrofanov <denis.mitr@gmail.com>
2018-08-07 19:20:14 +0300

    adds EventRecorderServiceProvider

--- src/EventRecorderServiceProvider.php
+++ src/EventRecorderServiceProvider.php
@@ -2,7 +2,23 @@
 
 namespace Denismitr\EventRecorder;
 
-class EventRecorderServiceProvider
-{
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
 
+class EventRecorderServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ( ! class_exists('CreateRecordedEventsTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_recorded_events_table.php.stub' =>
+                    database_path('migrations/' . date('Y_m_d_His', time()) . '_create_recorded_events_table.php'),
+            ], 'migrations');
+        }
+    }
+
+    public function register()
+    {
+        Event::subscribe(EventSubscriber::class);
+    }
 }
