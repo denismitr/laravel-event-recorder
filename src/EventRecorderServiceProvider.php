68a66dd5723c7c16c78417035a8738d8abff9ed3
Denis Mitrofanov <denis.mitr@gmail.com>
2018-08-08 21:56:34 +0300

    fixes service provider

--- src/EventRecorderServiceProvider.php
+++ src/EventRecorderServiceProvider.php
@@ -11,7 +11,7 @@ class EventRecorderServiceProvider extends ServiceProvider
     {
         if ( ! class_exists('CreateRecordedEventsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_recorded_events_table.php.stub' =>
+                __DIR__ . '/../database/migrations/create_recorded_events_table.php' =>
                     database_path('migrations/' . date('Y_m_d_His', time()) . '_create_recorded_events_table.php'),
             ], 'migrations');
         }
